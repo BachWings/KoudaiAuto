316152b6c11abb0def31c780c0225722d4a36530
Bach <Bachlee@foxmail.com>
2017-07-17 17:48:22 +0800

    部分路径修改

--- Public/ConfigParser.py
+++ Public/ConfigParser.py
@@ -6,6 +6,6 @@ import configparser
 
 def getConfig(section, key):
     config = configparser.ConfigParser()
-    config.read("E:\\Github\\KoudaiAuto\\PocketWallet\\PageObject\\PwProperties.ini", encoding="utf-8")
+    config.read("..\\PageObject\\PwProperties.ini", encoding="utf-8")
     return config.get(section, key)
 
